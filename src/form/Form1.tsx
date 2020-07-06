de5eec6ec4d3f890e012f3aeef865bb654b5dbb9
lynh.tang <lynh.tang@reisystems.com>
2020-07-06 18:24:29 -0400

    first commit

---
+++ src/form/Form1.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+export default class Form1 extends React.Component {
+
+    render() {
+        return (
+            <div>
+                this is form1
+            </div>
+        );
+    }
+}
